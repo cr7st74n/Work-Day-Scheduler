ce58790c2537aaf83643f204a0949210b7cd02d2
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2022-07-07 09:47:38 -0400

    Update the event listener and now is able to store the value in local storage.

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,54 @@ var pastImp1 = document.querySelector("#opt2");
 var pastImp2 = document.querySelector("#opt3");
 var pastImp3 = document.querySelector("#opt4");
 var pastImp4 = document.querySelector("#opt5");
-var button = document.querySelectorAll(".saveBtn")
-var FutureDate = document.querySelector(".future")
 
-console.log(FutureDate);
+var future1 = document.querySelectorAll(".future");
+var button = document.querySelectorAll(".saveBtn");
+
+
+button[5].addEventListener("click", function(event){
+    event.preventDefault();
+    future1[0].textContent = future1[0].value;
+    saveData(future1[0]);
+    getData();
+});
+function saveData(data){
+    localStorage.setItem("date",JSON.stringify(data.value));
+}
+
+function getData(){
+    var GetDate = localStorage.getItem("date");
+    if( GetDate !==null){
+        future1[0].value = GetDate;
+    }else{
+        return;
+    }
+    console.log(future1[0]);
+}
+
+
+
+button[6].addEventListener("click", function(){
+    future1[1].textContent = future1[1].value;
+    console.log(future1[1]);
+    saveData(future1[1]);
+
+});
+
+button[7].addEventListener("click", function(){
+    future1[2].textContent = future1[2].value;
+    console.log(future1[2]);
+    saveData(future1[2]);
+});
+button[8].addEventListener("click", function(){
+    future1[3].textContent = future1[3].value;
+    console.log(future1[3]);
+    saveData(future1[3]);
+});
+
+
+
+
 
 pastImp.disabled = true;
 pastImp1.disabled = true;
@@ -22,7 +66,15 @@ var dateToday  = "Date: "+(today.getMonth()+1)+"-"+today.getDate()+"-"+today.get
 dayCal.textContent = dateToday;
 
 // for (var i=0; button.length;i++){
-//     button[0].addEventListener("click",function(){
-//        // localStorage.setItem("saveDate","hola meco");
+//     button[i].addEventListener("click",function(){
+//         future1[i].textContent = future1[i].value;
+//         console.log(future1[i]);
 //     });
 // }
+function init(){
+    getData();
+}
+
+init();
+
+
