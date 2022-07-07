e07002213a856dc21f2bc940729d7d2ac2d78866
cr7st74n <cris-082011@hotmail.com>
2022-07-07 01:00:55 -0500

    Update: I have hadd new tags and a diferent Style in CSS, Also disable some imput tags that represent the past and present

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,28 @@
 var dayCal = document.querySelector("#currentDay");
+var pastImp = document.querySelector("#opt1");
+var pastImp1 = document.querySelector("#opt2");
+var pastImp2 = document.querySelector("#opt3");
+var pastImp3 = document.querySelector("#opt4");
+var pastImp4 = document.querySelector("#opt5");
+var button = document.querySelectorAll(".saveBtn")
+var FutureDate = document.querySelector(".future")
+
+console.log(FutureDate);
+
+pastImp.disabled = true;
+pastImp1.disabled = true;
+pastImp2.disabled = true;
+pastImp3.disabled = true;
+pastImp4.disabled = true;
+
 
 var today = new Date();
-
 var dateToday  = "Date: "+(today.getMonth()+1)+"-"+today.getDate()+"-"+today.getFullYear();
 
-console.log(dateToday);
-
 dayCal.textContent = dateToday;
+
+// for (var i=0; button.length;i++){
+//     button[0].addEventListener("click",function(){
+//        // localStorage.setItem("saveDate","hola meco");
+//     });
+// }
